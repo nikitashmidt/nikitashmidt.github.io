f6a4cca7f96536a170900db286d2161205dca9e1
Nikita Shmidt <nikitka.shmidt.01@gmail.com>
2022-07-20 03:17:33 +0600

    changes modal-comments

--- js/main.js
+++ js/main.js
@@ -387,7 +387,16 @@ document.addEventListener("DOMContentLoaded", () => {
           return;
         } else {
           item.comments.forEach((item, index) => {
-            let newItem = `<li id="${item.id}" class="modal-comments__item">${item.text}</li>`;
+            let newItem = `
+            <li id="${item.id}" class="modal-comments__item">
+              <div class="modal-comments__svg">
+                <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+              </div>
+              ${item.text}
+            </li>
+            `;
             modalCommentsItems.insertAdjacentHTML("beforeend", newItem);
           });
         }
@@ -395,7 +404,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     updateLocalStorage();
   }
-
   modalCommentsContent.addEventListener("touchstart", handleTouchStart, false);
   modalCommentsContent.addEventListener("touchmove", handleTouchMove, false);
   let xDown = null;
